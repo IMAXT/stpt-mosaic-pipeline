4967a20cd25e4de64c30120ab1dcc705684d2a4f
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2019-04-30 16:38:53 +0100

    Update pipeline schema

--- stpt_pipeline/schema.py
+++ stpt_pipeline/schema.py
@@ -1,3 +1,9 @@
 import voluptuous as vo
 
-schema = vo.Schema({vo.Required('datalen'): vo.Range(1, 1000)})
+schema = vo.Schema(
+    {
+        vo.Required('root_dir'): str,
+        vo.Required('output_dir'): str,
+        vo.Required('flat_file'): str,
+    }
+)
