6d2b6a36a675960cf9eedf9c958bbeb2a4a21962
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2019-05-15 15:20:45 +0100

    Update preprocessing step

--- stpt_pipeline/preprocess.py
+++ stpt_pipeline/preprocess.py
@@ -115,7 +115,28 @@ def list_directories(root_dir: Path):
     return dirs
 
 
-def save_image(filename, z, section, first_offset, layers):
+@retry(Exception)
+def save_image(filename, z, section, first_offset, fovs):
+    """[summary]
+
+    Parameters
+    ----------
+    filename : [type]
+        [description]
+    z : [type]
+        [description]
+    section : [type]
+        [description]
+    first_offset : [type]
+        [description]
+    fovs : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
     img = TiffImage(filename)
     match = (
         re.compile(r'(?P<offset>\d+)_(?P<channel>\d\d).tif$')
@@ -123,17 +144,36 @@ def save_image(filename, z, section, first_offset, layers):
         .groupdict()
     )
     offset, channel = int(match['offset']), match['channel']
-    fov = (offset - first_offset) // layers
-    zslice = (offset - first_offset) - fov * layers
-    g = z.create_group(f'section={section}/fov={fov}/z={zslice}/channel={channel}')
+    zslice = (offset - first_offset) // fovs
+    fov = offset - (first_offset + fovs * zslice)
+    try:
+        g = z.create_group(f'section={section}/fov={fov}/z={zslice}/channel={channel}')
+    except ValueError:
+        g = z[f'section={section}/fov={fov}/z={zslice}/channel={channel}']
     d = g.create_dataset('raw', data=img.asarray(), chunks=False)
-    print(d)
     return d
 
 
-def geom(d, flat=1):
+def apply_geometric_transform(d, flat):
+    """[summary]
+
+    Parameters
+    ----------
+    d : [type]
+        [description]
+    flat : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
     shapes = (Settings.x_max - Settings.x_min, Settings.y_max - Settings.y_min)
-    cropped = magic_function(d[:], flat=flat)
+    if flat is not None:
+        cropped = magic_function(d, flat=flat)
+    else:
+        cropped = d
     new = geometric_transform(
         cropped.astype('float32'),
         get_coords,
@@ -144,6 +184,25 @@ def geom(d, flat=1):
         order=1,
         prefilter=False,
     )
+    return new
+
+
+def geom(d, flat=1):
+    """[summary]
+
+    Parameters
+    ----------
+    d : [type]
+        [description]
+    flat : int, optional
+        [description], by default 1
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    new = apply_geometric_transform(d[:], flat)
 
     fh = zarr.group(store=d.store)
     path = d.path.replace('/raw', '')
@@ -151,13 +210,12 @@ def geom(d, flat=1):
     dd = g.create_dataset(
         'geom', data=new.astype('float32'), chunks=False, overwrite=True
     )
-    print(dd)
     return dd
 
 
 def preprocess(root_dir: Path, flat_file: Path, output_dir: Path):
     """[summary]
-    
+
     Parameters
     ----------
     root_dir : Path
@@ -166,7 +224,7 @@ def preprocess(root_dir: Path, flat_file: Path, output_dir: Path):
         [description]
     output_dir : Path
         [description]
-    
+
     Returns
     -------
     [type]
@@ -177,37 +235,35 @@ def preprocess(root_dir: Path, flat_file: Path, output_dir: Path):
 
     out = f'{output_dir / root_dir.name}.zarr'
     log.info('Storing images in %s', out)
-    z = zarr.group(out, 'w')
+    z = zarr.open(out, mode='a')
 
     dirs = list_directories(root_dir)
-    for d in dirs[:2]:
+    for d in dirs:
         # TODO: All this should be metadata
+        log.info('Preprocessing %s', d.name)
         section = re.compile(r'\d\d\d\d$').search(d.name).group()
+        if f'section={section}' in [name for name, group in z.groups()]:
+            log.info('Section %s already preprocessed. Skipping.', section)
+            continue
         mosaic = parse_mosaic_file(d)
         mrows, mcolumns = int(mosaic['mrows']), int(mosaic['mcolumns'])
         fovs = mrows * mcolumns
         files = sorted(list(d.glob('*.tif')))
-        files_ch1 = sorted(list(d.glob('*_01.tif')))
-        layers = len(files_ch1) // fovs
-        first_offset = int(
-            re.compile(r'-(\d+)_\d+.tif').search(files[0].name).groups()[0]
-        )
+        first_offset = [
+            int(re.compile(r'-(\d+)_\d+.tif').search(f.name).groups()[0]) for f in files
+        ]
+        first_offset = min(first_offset)
 
         res = []
         for f in d.glob('*.tif'):
-            r = delayed(save_image)(f, z, section, first_offset, layers)
+            r = delayed(save_image)(f, z, section, first_offset, fovs)
             g = delayed(geom)(r, flat=nflat)
             res.append(g)
 
         client = Client.current()
         fut = client.compute(res)
         wait(fut)
+        z[f'section={section}'].attrs.update(mosaic)
+        z[f'section={section}'].attrs['fovs'] = fovs
 
-    # Write metadata
-    # TODO: Write contents of mosaic file here as well
-    z.attrs['sections'] = len(dirs)  # TODO: Read from mosaic file
-    z.attrs['fovs'] = fovs
-    z.attrs['mrows'] = mrows
-    z.attrs['mcolumns'] = mcolumns
-    z.attrs['layers'] = layers
     return z
