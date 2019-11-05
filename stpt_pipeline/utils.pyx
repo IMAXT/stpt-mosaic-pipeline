87daf79619b69dfd40a4067929bd24f848d48888
Carlos Gonzalez-Fernandez <cgonzal@ast.cam.ac.uk>
2019-11-05 10:04:57 +0000

    Refined geometric transform, added different scales for x and y

--- stpt_pipeline/utils.pyx
+++ stpt_pipeline/utils.pyx
@@ -1,4 +1,4 @@
-from libc.math cimport sqrt, pow
+from libc.math cimport abs
 
 def get_coords(tuple coords, list cof_x, list cof_y, list tan, float normal_x, float normal_y):
     """[summary]
@@ -21,11 +21,11 @@ def get_coords(tuple coords, list cof_x, list cof_y, list tan, float normal_x, f
     cdef int c0, c1
     cdef tuple res
     cdef float x_adim, y_adim, distorted_x, distorted_y, xx, yy
-    cdef float xc0, xc1, xc2, xc3, xc4, yc0, yc1, yc2, yc3, yc4, t0, t1
+    cdef float xc0, xc1, xc2, xc3, yc0, yc1, yc2, yc3, t0, t1
 
     c0, c1 = coords
-    xc0, xc1, xc2, xc3, xc4 = cof_x
-    yc0, yc1, yc2, yc3, yc4 = cof_y
+    xc0, xc1, xc2, xc3 = cof_x
+    yc0, yc1, yc2, yc3 = cof_y
     t0, t1 = tan
 
     x_adim = (c0 - t0) / normal_x
@@ -33,8 +33,8 @@ def get_coords(tuple coords, list cof_x, list cof_y, list tan, float normal_x, f
 
     xx = x_adim*x_adim
     yy = y_adim*y_adim
-    distorted_x = c0 + x_adim*(yy*xc0 + xx*xc1 + xx*y_adim*xc2 + xx*yy*xc3 + xx*xx*xc4)
-    distorted_y = c1 + y_adim*(xx*yc0 + yy*yc1 + yy*x_adim*yc2 + yy*xx*yc3 + yy*yy*yc4)
+    distorted_x = c0 + xc0*x_adim/abs(x_adim) + x_adim*(yy*xc1 + xx*xc2 + xx*y_adim*xc3)
+    distorted_y = c1 + yc0*y_adim/abs(y_adim) + y_adim*(xx*yc1 + yy*yc2 + yy*x_adim*yc3)
 
     res = (distorted_x, distorted_y)
     return res
