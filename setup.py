834223e2e41ab43c135d6d86148ba3b44d6d1f43
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2020-05-28 14:51:28 +0100

    Change strings to double quotes

--- setup.py
+++ setup.py
@@ -2,59 +2,59 @@ from Cython.Build import cythonize
 from setuptools import find_packages, setup
 from setuptools.extension import Extension
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'dask',
-    'distributed',
-    'imaxt-image',
-    'zarr',
-    'numpy',
-    'scipy',
-    'voluptuous',
-    'xarray',
-    'opencv-python',
+    "dask",
+    "distributed",
+    "imaxt-image",
+    "zarr",
+    "numpy",
+    "scipy",
+    "voluptuous",
+    "xarray",
+    "opencv-python",
 ]
 
-setup_requirements = ['pytest-runner', 'flake8']
+setup_requirements = ["pytest-runner", "flake8"]
 
-test_requirements = ['coverage', 'pytest', 'pytest-cov', 'pytest-mock']
+test_requirements = ["coverage", "pytest", "pytest-cov", "pytest-mock"]
 
 setup(
-    author='Carlos Gonzalez',
-    author_email='cgonzal@ast.cam.ac.uk',
+    author="Carlos Gonzalez",
+    author_email="cgonzal@ast.cam.ac.uk",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
-    description='STPT pipeline.',
-    entry_points={'owl.pipelines': 'stpt = stpt_pipeline'},
+    description="STPT pipeline.",
+    entry_points={"owl.pipelines": "stpt = stpt_pipeline"},
     install_requires=requirements,
-    license='GNU General Public License v3',
-    long_description=readme + '\n\n' + history,
+    license="GNU General Public License v3",
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    data_files=[('conf', ['conf/stpt_pipeline.yaml'])],
-    keywords='stpt_pipeline',
-    name='stpt_pipeline',
-    packages=find_packages(include=['stpt_pipeline']),
+    data_files=[("conf", ["conf/stpt_pipeline.yaml"])],
+    keywords="stpt_pipeline",
+    name="stpt_pipeline",
+    packages=find_packages(include=["stpt_pipeline"]),
     ext_modules=cythonize(
-        Extension('stpt_pipeline.utils', ['stpt_pipeline/utils.pyx'])
+        Extension("stpt_pipeline.utils", ["stpt_pipeline/utils.pyx"])
     ),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://gitlab.ast.cam.ac.uk/imaxt/stpt_pipeline',
-    version='0.3.0',
+    url="https://gitlab.ast.cam.ac.uk/imaxt/stpt_pipeline",
+    version="0.3.0",
     zip_safe=False,
     dependency_links=[
-        'https://imaxt.ast.cam.ac.uk/pip/imaxt-image',
-        'https://imaxt.ast.cam.ac.uk/pip/zarr',
+        "https://imaxt.ast.cam.ac.uk/pip/imaxt-image",
+        "https://imaxt.ast.cam.ac.uk/pip/zarr",
     ],
 )
