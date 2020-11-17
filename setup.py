9e61353b97e1d4215f10a485973876b73f8a17c3
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2020-11-17 16:41:27 +0000

    Bump version: 0.5.1 → 0.5.6

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.ast.cam.ac.uk/imaxt/stpt_pipeline",
-    version="0.5.1",
+    version="0.5.6",
     zip_safe=False,
     dependency_links=[
         "https://imaxt.ast.cam.ac.uk/pip/imaxt-image",
