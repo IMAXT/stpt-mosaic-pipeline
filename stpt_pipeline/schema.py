3d002e5ee763fd3182e7493689bfd337f673820a
Carlos Gonzalez-Fernandez <cgonzal@ast.cam.ac.uk>
2021-04-26 10:26:43 +0100

    Added cals to schema

--- stpt_pipeline/schema.py
+++ stpt_pipeline/schema.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import voluptuous as vo
 
-DEFAULT_RECIPES = ["flat", "mosaic", "downsample", "beadreg"]
+DEFAULT_RECIPES = ["cals", "mosaic", "downsample", "beadreg"]
 
 
 def check_recipes(val):
