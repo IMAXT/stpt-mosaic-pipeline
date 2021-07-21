ea25ce34fdd638d922a14f5804e01f12f5835391
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-07-21 11:55:37 +0000

    Bump version: 0.7.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.ast.cam.ac.uk/imaxt/stpt_pipeline",
-    version="0.7.0",
+    version="0.9.0",
     zip_safe=False,
     dependency_links=[
         "https://imaxt.ast.cam.ac.uk/pip/imaxt-image",
--- stpt_pipeline/__init__.py
+++ stpt_pipeline/__init__.py
@@ -1,4 +1,4 @@
 from .main import main  # noqa: F401
 from .schema import schema  # noqa: F401
 
-__version__ = "0.7.0"
+__version__ = "0.9.0"
